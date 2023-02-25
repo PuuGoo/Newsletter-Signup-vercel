a806eae43436a8c3ad020f157b17e48cb5288db9
PuuGoo <ndphu1997@gmail.com>
2023-02-25 12:25:16 +0700

    Change 1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
